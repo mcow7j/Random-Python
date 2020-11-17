6f71571d0264544a4646c921ee6e0914b7d49601
Matthew Cowley <cowley7j@gmail.com>
2020-11-17 13:50:14 +0000

    Update coursework4 fin.py

--- coursework4 fin.py
+++ coursework4 fin.py
@@ -1,43 +1,26 @@
-9655020917106901547888376921602009
+from math import log10
 
-n=99158679366511727*97369398007206967
-n=9655020917106901547888376921602009
+def digit_count(a):
+    z=log10(a)
+    return int(z)+1
 
-e = 43
-
-#ist1=map(lambda c: ord(c)-32,'My name is Bond')
-#str1=""
-#for n in xrange(1,len(list1)+1):
- #   if list1(n)==0:
-  #      str1=str1+'00'
-   # else:
-    #    w=list1(n)
-     #   str2=
 def cypherconverter(str1):
     list1=map(lambda c: ord(c)-32, str1)
     p=0       
     for n in xrange(1,len(list1)+1):
         w=len(list1)
         p=(list1[w-n]*(10**(2*(n-1))))+p
-    print p
+    return p
  
-def encoder2(str1,e,n):
+def encoder(str1,e,n):
     list1=map(lambda c: ord(c)-32, str1)
     p=0       
     for n in xrange(1,len(list1)+1):
         w=len(list1)
         p=(list1[w-n]*(10**(2*(n-1))))+p
-    pow(p,e,n)
-    
-from math import log10
-
-def digitcount(a):
-    z=log10(a)
-    return int(z)+1
-    
-def decode6(Q):
-    d=4715242773470812287873634662919759
-    n=9655020917106901547888376921602009
+    return pow(p,e,n)
+       
+def decode(Q,n,d):
     p = pow(Q,d,n)
     string1 = ''
     for t in xrange(1,((digit_count(p)/2)+1)):
@@ -45,9 +28,8 @@ def decode6(Q):
         r = (p/(10**(w*2)))-100*(p/(10**((w*2)+2)))
         string1 = string1 + chr(r+32)
     return string1
-
-
-
-def digit_count(a):
-    z=log10(a)
-    return int(z)+1
+   
+if __name__ == '__main__':
+  n = 9655020917106901547888376921602009
+  d = 4715242773470812287873634662919759
+  e = 43
