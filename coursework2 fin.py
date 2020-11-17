d7e28a3fbc5603cafd194c32c05e7b3a3ee5b908
Matthew Cowley <cowley7j@gmail.com>
2020-11-17 13:31:19 +0000

    Update coursework2 fin.py

--- coursework2 fin.py
+++ coursework2 fin.py
@@ -1,10 +1,12 @@
+""" module with 2 different functions to find the highest common factor between 2 intergers a,b """
+
+from itertools import count
+
 def hcf1(a, b):
     """finds highest common factor between a and b"""
     while b>0:
         a, b = b, a % b
     return a
-    
-from itertools import count
 
 def hcf2(a, b):
     """finds highest common factor between a and b"""
@@ -12,14 +14,3 @@ def hcf2(a, b):
         a, b = b, a % b
         if b == 0:
             return a
-
-def ehcf(a, b):
-    """finds highest common factor between a and b"""
-    p1,q1,h1,p2,q2,h2=1,0,a,0,1,b
-    while h2>0:
-        r=h1/h2
-        p3=p1-r*p2
-        q3=q1-r*q2
-        h3=h1-r*h2
-        p1,q1,h1,p2,q2,h2=p2,q2,h2,p3,q3,h3
-    return (p1,q1,h1)
